a61108d3b9199411f023f3711fe9b8b53cafedbe
Michael Silva <michael.steam@outlook.com>
2022-03-07 22:36:39 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 lados para um triângulo");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if( (a == b) && (b == c) )
+            {
+                Console.WriteLine("Triângulo Equilátero");
+            }else if( (a != b) && (b != c) && (a != c))
+            {
+                Console.WriteLine("Triângulo Escaleno");
+            }
+            else
+            {
+                Console.WriteLine("Triâgulo Isosceles");
+            }
+            Console.ReadLine();
+        }
+    }
+}
